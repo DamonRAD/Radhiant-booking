30cce126f94c4c0ccde07f07f32de17520e0d9f0
DamonRAD <damondr@radhiant.com>
2025-06-17 11:59:38 +0200

    Update route.js

--- app/api/graph/route.js
+++ app/api/graph/route.js
@@ -1,1 +1,7 @@
+// app/api/graph/route.js
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  return NextResponse.json({ message: "Hello from Microsoft Graph API" });
+}
 
